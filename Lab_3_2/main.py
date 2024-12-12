7407b2bdef6503d57fddf43483e1fbf8a605fc94
roale <rom3rom@gmail.com>
2024-12-12 17:02:46 +0200

    Lab-2

---
+++ Lab_3_2/main.py
@@ -0,0 +1,5 @@
+#uznat vxodit li slova v stro4ku
+
+my_string = "Hello world"
+
+print("Hello" in my_string)
---
+++ Lab_3_3/main.py
@@ -0,0 +1,24 @@
+# 1 Napisanit stro4i UPPER
+
+my_sting = "Roman"
+
+print(my_sting.upper())
+
+# 2 Napisat sto4iku low
+
+print(my_sting.lower())
+
+# 3 ubrat probeli
+
+my_new_sting = "  Hello World   "
+print(len(my_new_sting))
+
+print(len(my_new_sting.strip()))
+
+# 4 Zamena slova v stro4ki
+
+print(my_sting.replace("Roman","Python"))
+
+# 5 pos4itat koli4sto bukv v storke
+
+
